e111a644be18ddda02aaed9f4423467a1a27cc61
Damion Morgan <only4school5680@gmail.com>
2024-06-26 16:15:23 -0400

    featured products done but the buttons are at the same height

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,63 @@
 import Image from "next/image";
+import type { StaticImageData } from "next/image";
 //Banner Images
 import IphoneImage from "@/public/homePageIcons/Iphone.png"
 import P5 from "@/public/homePageIcons/p5.png"
 import airpodMax from "@/public/homePageIcons/airpodMax.png"
 import visionPro from "@/public/homePageIcons/visionPro.png"
 import macbookAir from "@/public/homePageIcons/macbookAir.png"
+
 //Catergory Nav Icons
-import arrows from "@/public/homePageIcons/Arrows.png"
+import arrowLeft from "@/public/homePageIcons/arrowLeft.png"
+import arrowRight from "@/public/homePageIcons/arrowRight.png"
 import camera from  "@/public/homePageIcons/Cameras.png"
 import watch from  "@/public/homePageIcons/SmartWatches.png"
 import computer from  "@/public/homePageIcons/Computers.png"
 import gaming from  "@/public/homePageIcons/Gaming.png"
 import headphone from  "@/public/homePageIcons/Headphones.png"
 import phone from  "@/public/homePageIcons/Phones.png"
+
+//Featured Images
+import cannonCamera from "@/public/homePageIcons/featuredIcons/cannonCamera.png";
+import Iphone1pro from "@/public/homePageIcons/featuredIcons/Iphone1 pro.png";
+import appleWatch from "@/public/homePageIcons/featuredIcons/appleWatch.png";
+import airpodsMax from "@/public/homePageIcons/featuredIcons/airpodsMax.png";
+import galaxyWatch from "@/public/homePageIcons/featuredIcons/galaxyWatch.png";
+import galaxyFold from "@/public/homePageIcons/featuredIcons/galaxyFold.png";
+import galaxyBuds from "@/public/homePageIcons/featuredIcons/galaxyBuds.png";
+import appleIpad from "@/public/homePageIcons/featuredIcons/appleIpad.png";
+import favBlank from "@/public/homePageIcons/featuredIcons/favBlank.png";
+import favFull from "@/public/homePageIcons/featuredIcons/favFull.png";
+
+
+class Product {
+  name: string;
+  description: string;
+  price: number;
+  hearted: boolean;
+  imgSrc: StaticImageData;
+
+  constructor(name: string, description: string, price: number, hearted: boolean, imgSrc: any) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.hearted = hearted;
+    this.imgSrc = imgSrc;
+  }
+}
+
+// Create instances of the Product class
+const featuredProducts = [
+  new Product("Apple iPhone 14 Pro Max", "128GB Deep Purple", 900, false, Iphone1pro),
+  new Product("Blackmagic Pocket Cinema", "Camera 6k", 2535, false, cannonCamera),
+  new Product("Apple Watch Series 9 GPS", "128GB Deep Purple", 900, false, appleWatch),
+  new Product("AirPods Max Silver", "Starlight Aluminium", 549, false, airpodsMax),
+  new Product("Samsung Galaxy Watch6", "128GB Deep Purple", 900, false, galaxyWatch),
+  new Product("Galaxy Z Fold5 Unlocked", "256GB | Phantom Black", 1799, true, galaxyFold),
+  new Product("Galaxy Buds FE", "Graphite", 99.99, false, galaxyBuds),
+  new Product("Apple iPad 9 10.2 64GB Wi-Fi", "Silver (MK2L3) 2021", 398, false, appleIpad)
+];
+
 
 export default function Home() {
   return (
@@ -93,35 +138,38 @@ export default function Home() {
 
       {/* Catergory Nav */}
       <section className="bg-[#FAFAFA]">
-      <div className="flex justify-between py-12 px-36">
+      <div className="flex justify-between py-12 px-36 text-[24px]">
         <p>Browse By Catergory</p>
-        <Image src={arrows} alt='Navigation Arrows'></Image>
+        <div className="flex">
+           <Image className='mx-4' src={arrowLeft} alt='Navigation Arrows' width={24} height={24}></Image>
+          <Image className='mx-4' src={arrowRight} alt='Navigation Arrows' width={24} height={24}></Image>
+        </div>
       </div>
 
-      <nav className="list-none flex  px-36 ">
+      <nav className="list-none flex justify-center">
         <li className=" h-[128px] bg-[#EDEDED] rounded-xl flex-col py-8 px-9 mx-10" >
-          <Image className="mx-3" src={phone} alt="Phone" width={48} height={48}></Image>
-          <p className="text-[24px]">Phones</p>
+          <Image className="mx-1" src={phone} alt="Phone" width={48} height={48}></Image>
+          <p className="text-[16px]">Phones</p>
         </li>
         <li className="h-[128px] bg-[#EDEDED] rounded-xl flex-col py-8 px-9  mx-10">
-          <Image className="mx-5" src={watch} alt="watch" width={48} height={48}></Image>
-          <p className="text-[24px]">Watches</p>
+          <Image className="mx-2" src={watch} alt="watch" width={48} height={48}></Image>
+          <p className="text-[16px]">Watches</p>
         </li>
         <li className="h-[128px] bg-[#EDEDED] rounded-xl flex-col py-8 px-9  mx-10">
-          <Image className="mx-5" src={camera} alt='camera' width={48} height={48}></Image>
-          <p className="text-[24px]">Cameras</p>
+          <Image className="mx-3" src={camera} alt='camera' width={48} height={48}></Image>
+          <p className="text-[16px]">Cameras</p>
         </li>
         <li className=" h-[128px] bg-[#EDEDED] rounded-xl flex-col py-8 px-9  mx-10 w-fit">
-          <Image className="mx-10" src={headphone} alt="headphone" width={48} height={48}></Image>
-          <p className="text-[24px]">Headphones</p>
+          <Image className="mx-4" src={headphone} alt="headphone" width={48} height={48}></Image>
+          <p className="text-[16px]">Headphones</p>
         </li>
         <li className=" h-[128px] bg-[#EDEDED] rounded-xl flex-col py-8 px-9  mx-10 w-fit">
-          <Image className="mx-10" src={computer} alt="computer" width={48} height={48}></Image>
-          <p className="text-[24px]">Computers</p>
+          <Image className="mx-4" src={computer} alt="computer" width={48} height={48}></Image>
+          <p className="text-[16px]">Computers</p>
         </li>
         <li className=" h-[128px] bg-[#EDEDED] rounded-xl flex-col py-8 px-9  mx-10">
-          <Image className="mx-3" src={gaming} alt="gaming" width={48} height={48}></Image>
-          <p className="text-[24px]">Gaming</p>
+          <Image className="mx-2" src={gaming} alt="gaming" width={48} height={48}></Image>
+          <p className="text-[16px]">Gaming</p>
         </li>
       </nav>
       </section>
@@ -129,11 +177,23 @@ export default function Home() {
 
       {/* Results */}
       <section>
-        <nav>
-
+        <nav className="flex list-none  py-12 text-[18px] mx-auto  w-[1120px]">
+          <li className="mx-2 border-b-[3px] border-black">New Arrival</li>
+          <li className="mx-2 text-[#8B8B8B]" >Bestseller</li>
+          <li className="mx-2 text-[#8B8B8B]" >Feature Products</li>
         </nav>
-        <div>
 
+        <div className="grid grid-cols-4 grid-rows-2 gap-2 w-[1120px] mx-auto">
+          {featuredProducts.map((product, index) => (
+                <div className='bg-[#F6F6F6] flex-col mx-auto my-auto p-8 w-[268px] h-[432px]' key={index}>
+                 <Image src={product.hearted ? favFull : favBlank} alt='favorite icon' height={32} width={32} className=""/>
+                  <Image className="mx-auto " src={product.imgSrc} alt={product.name} height={160} width={160} />
+                  <p className="text-[16px] text-center">{product.name}</p>
+                  <p className="text-[16px] text-center ">{product.description}</p>
+                  <p className="text-[24px] font-bold text-center">${product.price}</p>
+                  <button className="border border-black bg-black text-white rounded-md text-[14px] px-10 py-2 w-[188px] mx-auto ">Buy Now</button>
+                </div>
+              ))}
         </div>
       </section>
 
